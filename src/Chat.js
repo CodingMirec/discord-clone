bd090739218a748b66e8e4cefb1e433013c1fae5
CodingMirec <m.sykora01@gmail.com>
2020-10-25 19:02:42 +0100

    chat part -- chat header

---
+++ src/Chat.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ChatHeader from "./ChatHeader";
+
+import "./Chat.css";
+
+function Chat() {
+  return (
+    <div className="chat">
+      <ChatHeader />
+    </div>
+  );
+}
+
+export default Chat;
